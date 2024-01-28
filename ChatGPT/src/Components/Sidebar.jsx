348bb6f556f92b33fd60d3ba4b881e54bd6d7bc2
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2024-01-29 00:12:31 +0530

    Update Sidebar.jsx

--- ChatGPT/src/Components/Sidebar.jsx
+++ ChatGPT/src/Components/Sidebar.jsx
@@ -3,13 +3,24 @@ import React from 'react'
 const Sidebar = () => {
   return (
     <div className='sidebarsection'>
-        <div className="upperbody">
+      <div className="upperbody">
+        <div className="upperbody1">
+          <img src="/src/assets/chatgpt.svg" alt="gptlogo" className="chatgptlogo" />
+          <span className="appname">ChatGPT</span>
+          </div>
+          <button className="newchat">
+            <img src="" alt="" className="add" />
+            New Chat
+          </button>
+        <div className="upperbody2">
+           <button className="query1"><img src="" alt="" />What is an Api ?</button>
+           <button className="query2"><img src="" alt="" />How to learn Programming ?</button>
+        </div>
+      </div>
+      <div className="lowerbody">
 
-        </div>
-        <div className="lowerbody">
-            
-        </div>
-      
+      </div>
+
     </div>
   )
 }
