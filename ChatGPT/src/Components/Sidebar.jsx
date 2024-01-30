5b0e2253e2a5a0fc408df6529839860e92b93564
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2024-01-30 12:46:43 +0530

    Update Sidebar.jsx

--- ChatGPT/src/Components/Sidebar.jsx
+++ ChatGPT/src/Components/Sidebar.jsx
@@ -8,17 +8,30 @@ const Sidebar = () => {
           <img src="/src/assets/chatgpt.svg" alt="gptlogo" className="chatgptlogo" />
           <span className="appname">ChatGPT</span>
           </div>
+          <div className="btn">
           <button className="newchat">
-            <img src="" alt="" className="add" />
+            <img src="/src/assets/add-30.png" alt="" className="add" />
             New Chat
           </button>
+          </div>
         <div className="upperbody2">
-           <button className="query1"><img src="" alt="" />What is an Api ?</button>
-           <button className="query2"><img src="" alt="" />How to learn Programming ?</button>
+           <button className="query1"><img src="/src/assets/message.svg" alt="" />What is an Api ?</button>
+           <button className="query2"><img src="/src/assets/message.svg" alt="" />How to learn Programming ?</button>
         </div>
       </div>
       <div className="lowerbody">
-
+         <div className="home">
+          <img src="/src/assets/home.svg" alt="" />
+          Home
+         </div>
+         <div className="saved">
+          <img src="/src/assets/bookmark.svg" alt="" />
+          Saved
+         </div>
+         <div className="upgrade">
+          <img src="/src/assets/rocket.svg" alt="" />
+          Upgrade to Pro
+         </div>
       </div>
 
     </div>
