8ece47f90f81d9dd09231d287deac2ae85405a7a
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2024-02-02 02:58:53 +0530

    Update App.jsx

--- ChatGPT/src/App.jsx
+++ ChatGPT/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-
 import './App.css'
 import Sidebar from './Components/Sidebar'
-import Mainbody from './Components/mainbody'
+import Mainbody from './Components/Mainbody'
+import {FaBars,FaTimes,FaArrowRight, FaArrowLeft,FaList} from 'react-icons/fa'
 
 function App() {
+  const [bars,Setbars]=useState(false);
+  const showsidebar=()=>{
+    Setbars(!bars);
+  }
   return (
     <>
-    <div className="container">
-      <Sidebar/>
+    <div className="container" >
+      {bars?<button className='openbtn' onClick={showsidebar}>
+        <FaTimes size="20"/>
+        </button>
+      :<button className='closebtn' onClick={showsidebar}>
+        <FaList size="20"/>
+      </button>}
+      {bars?<Sidebar/>:""}
+      
       <Mainbody/>
     </div>
     </>
